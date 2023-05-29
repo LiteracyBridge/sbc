19b96b9ac244eb09effa6b821defe230939cd8de
Lawrence Ephrim <ephrimlawrence@gmail.com>
2023-05-29 08:32:15 +0000

    feat(toc: indicators): implement auto-updating browser panel with existing toc indicators

--- frontend/src/views/theory-of-change/IndicatorBrowserPanel.vue
+++ frontend/src/views/theory-of-change/IndicatorBrowserPanel.vue
@@ -1,9 +1,8 @@
 <script lang="ts" setup>
-import { ref, reactive, onMounted, onBeforeUnmount, computed, watch } from "vue";
+
+import { ref, onMounted, computed, watch } from "vue";
 import { Multiselect } from 'vue-multiselect'
 import { AccordionList, AccordionItem } from "vue3-rich-accordion";
-
-import { useProjectStore } from "@/stores/projects";
 import { ApiRequest } from "@/apis/api";
 import { type IndicatorGroup, type IndicatorType, TheoryOfChangeItem, TheoryOfChange } from "@/types";
 import { Empty } from "ant-design-vue";
@@ -16,10 +15,10 @@ const emit = defineEmits<{
 
 const props = defineProps<{ tocItem: TheoryOfChangeItem, isVisible: boolean }>();
 
-const selectedIndicatorType = ref<{ id: number, name: string }>(),
-  selectedGroup = ref<IndicatorType>(),
+const selectedMainIndicatorType = ref<{ id: number, name: string }>(),
+  selectedGroupType = ref<IndicatorType>(),
   isFetchingIndicators = ref(false),
-  indicatorTypes = ref<IndicatorType[]>([]),
+  mainIndicators = ref<IndicatorType[]>([]),
   indicatorsList = ref<IndicatorGroup[]>([]),
   config = ref<{
     isLoading: boolean,
@@ -29,60 +28,34 @@ const selectedIndicatorType = ref<{ id: number, name: string }>(),
     indicators: {},
   });
 
-// const tocItemIndicators = ref(props.tocItem.indicators ?? []);
-
 const isOpened = computed(() => props.isVisible)
 
-
 const getMainIndicators = computed(() => {
-  return indicatorTypes.value.filter(i => i.parent_id == null);
+  return mainIndicators.value.filter(i => i.parent_id == null);
 });
 
 const selectedIndicatorGroups = computed(() => {
-  console.log(selectedIndicatorType.value);
+  console.log(selectedMainIndicatorType.value);
 
-  if (selectedIndicatorType?.value == null) {
+  if (selectedMainIndicatorType?.value == null) {
     return [];
   }
 
-  return indicatorTypes.value.filter(i => i.parent_id == selectedIndicatorType.value.id);
+  return mainIndicators.value.filter(i => i.parent_id == selectedMainIndicatorType.value.id);
 });
 
 const groupIndicators = computed(() => {
-  if (selectedGroup.value?.id == null) {
+  if (selectedGroupType.value?.id == null) {
     return [];
   }
 
-  return indicatorsList.value.filter(i => i.group_id == selectedGroup.value?.id);
+  return indicatorsList.value.filter(i => i.group_id == selectedGroupType.value?.id);
 })
-
-// const tocIte = computed(() => {
-//   if (selectedGroup.value?.id == null) {
-//     return [];
-//   }
-
-//   return indicatorsList.value.filter(i => i.group_id == selectedGroup.value?.id);
-// })
-
-function onIndicatorSelected(item: any, _: any) {
-  // const temp = indicatorsAdded.value;
-  // temp.push(item.id)
-
-  // indicatorsAdded.value = temp;
-  // console.log(temp)
-}
 
 const closePanel = () => {
   config.value.indicators = {};
-  // isOpened.value = false;
-
-  console.error("emitted")
   emit("isClosed", true);
 };
-
-// const tocItemHasIndicators = computed<boolean>(() => {
-//   return props.tocItem.indicators.length > 0;
-// })
 
 const buildIndicatorsTree = (indicators: any[]) => {
   const tree: Record<string, boolean> = {};
@@ -97,15 +70,13 @@ const buildIndicatorsTree = (indicators: any[]) => {
 onMounted(() => {
   buildIndicatorsTree(props.tocItem?.indicators ?? []);
 
-  // selectedGroup.value = getMainIndicators.value[0];
-
   isFetchingIndicators.value = true;
   Promise.all([
     ApiRequest.get<IndicatorType[]>("indicators/types")
       .then((resp) => {
         console.log(resp)
         if (resp != null) {
-          indicatorTypes.value = resp;
+          mainIndicators.value = resp;
         }
       }),
 
@@ -123,31 +94,20 @@ onMounted(() => {
     .finally(() => isFetchingIndicators.value = false);
 })
 
-// watch(config, (newConfig, _oldConfig) => {
-//   buildIndicatorsTree(newConfig.indicators)
-// }, { deep: true });
 
 watch(props, (newProp) => {
   buildIndicatorsTree(newProp.tocItem.indicators)
 
-  // TODO: auto select indicator type & group
+  // Update panel with ToC item existing indicator
+  const _indicators = newProp.tocItem?.indicators ?? [];
+  if (_indicators.length > 0) {
+    const indicator = indicatorsList.value.find(i => i.id == _indicators[0].indicator_id);
+    const indicatorType = mainIndicators.value.find(i => i.id == indicator?.group_id);
+
+    selectedGroupType.value = indicatorType;
+    selectedMainIndicatorType.value = mainIndicators.value.find(i => i.id == indicatorType?.parent_id);
+  }
 }, { deep: true });
-
-// const removeIndicator = (indicatorId: number) => {
-//   const temp = config.indicatorsRemoved
-//   temp.push(indicatorId)
-
-//   config.indicatorsRemoved = temp;
-
-//   const _temp2 = config.indicatorsAdded;
-//   const index = _temp2.findIndex(i => i == indicatorId);
-//   if (index > -1) {
-//     _temp2.splice(index, 1)
-//     config.indicatorsAdded = _temp2
-//   }
-
-//   buildIndicatorsTree()
-// }
 
 
 const saveIndicators = async () => {
@@ -160,18 +120,11 @@ const saveIndicators = async () => {
   const added = Object.keys(config.value.indicators).filter(i => {
     return config.value.indicators[i]
   }).filter(i => props.tocItem.indicators.find(ind => ind.indicator_id == +i) == null)
-  // Filter out duplicate indicators
-  // const uniqueAdded = props.tocItem.indicators.fi
-
-  console.log(added, removed)
-  // TODO: if item is not new, then update it
 
   await ApiRequest.post<TheoryOfChange>(`theory-of-change/${props.tocItem.id}/indicators`, {
     added: added,
     removed: removed,
   }).then(resp => {
-    console.log(resp)
-
     emit("isSaved", resp);
     closePanel();
   }).finally(() => {
@@ -179,7 +132,6 @@ const saveIndicators = async () => {
   });
 }
 
-// TODO: watch for config.indicators changes and update UI
 </script>
 
 <template>
@@ -192,19 +144,18 @@ const saveIndicators = async () => {
         <!-- TODO: add label -->
         <label class="label">Select Indicator</label>
 
-        <Multiselect v-model="selectedIndicatorType" :options="getMainIndicators" :close-on-select="true"
-          :clear-on-select="false" placeholder="Select indicator" label="name" track-by="value"
-          @select="onIndicatorSelected" />
+        <Multiselect v-model="selectedMainIndicatorType" :options="getMainIndicators" :close-on-select="true"
+          :clear-on-select="false" placeholder="Select indicator" label="name" track-by="value" />
         <hr>
 
         <aside class="menu">
           <p class="menu-label">
-            {{ selectedIndicatorType?.name || 'No indicatory type selected' }}
+            {{ selectedMainIndicatorType?.name || 'No indicatory type selected' }}
           </p>
 
           <ul class="menu-list">
             <li v-for="indicator in selectedIndicatorGroups" :key="indicator.id">
-              <a @click.prevent="selectedGroup = indicator">{{ indicator.name }}</a>
+              <a @click.prevent="selectedGroupType = indicator">{{ indicator.name }}</a>
             </li>
           </ul>
         </aside>
@@ -219,17 +170,14 @@ const saveIndicators = async () => {
             <div class="level-item">
 
               <!-- <div class="container mb-4"> -->
-              <h3 class="has-text-weight-bold">{{ selectedGroup?.name || '' }}</h3>
+              <h3 class="has-text-weight-bold">{{ selectedGroupType?.name || '' }}</h3>
               <!-- </div> -->
             </div>
           </div>
 
           <div class="level-right">
             <div class="level-item">
-              <!-- <button class="button is-primary">
-                                                                      Save
-                                                                    </button>
-                                                                  -->
+
               <button class="button mr-2 is-primary" :class="{ 'is-loading': config.isLoading }"
                 :disabled="config.isLoading" @click.prevent="saveIndicators">
                 Save
@@ -241,12 +189,8 @@ const saveIndicators = async () => {
             </div>
           </div>
 
-
-
         </div>
         <hr>
-
-        <!-- TODO: show a helper text in center if nothing is selected -->
 
         <div v-if="groupIndicators?.length == 0" style="margin-top: auto; position: fixed; top: 50%; left: 55%;">
           <Empty description="Choose indicator from the dropdown list"></Empty>
@@ -282,15 +226,6 @@ const saveIndicators = async () => {
               </div>
 
               <footer class="card-footer">
-                <!-- <p class="card-footer-item" v-if="!config.indicators[`${item.id}`] == false">
-                                <button class="button is-primary" @click="config.indicators[`${item.id}`] = true">
-                                  <span class="icon mr-1">
-                                    <i class="fas fa-plus"></i>
-                                  </span>
-                                  Add Indicator
-                                </button>
-                              </p> -->
-
                 <p class="card-footer-item">
                   <button class="button"
                     :class="{ 'is-danger': config.indicators[`${item.id}`], 'is-primary': !config.indicators[`${item.id}`] }"
