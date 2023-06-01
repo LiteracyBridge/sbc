211faa440687cd15a82a834add60d20cdf5a779e
Lawrence Ephrim <ephrimlawrence@gmail.com>
2023-06-01 09:02:11 +0000

    refactor: redesigned approaches UI with collapse component

--- frontend/src/views/Interventions.vue
+++ frontend/src/views/Interventions.vue
@@ -1,93 +1,64 @@
-<script setup>
-import { useDriverStore } from "../stores/drivers";
-import { useLookupStore } from "../stores/lookups";
+<script lang="ts" setup>
 import { useActivityStore } from "../stores/activities";
 import { useInterventionStore } from "../stores/interventions";
-import { ref, onMounted } from "vue";
-import { useUserStore } from "@/stores/user";
-import { useRouter } from 'vue-router'
+import { ref, computed } from "vue";
+import { Button, Collapse, CollapsePanel } from "ant-design-vue";
+import { CheckCircleOutlined, DeleteOutlined } from "@ant-design/icons-vue";
 
-const userStore = useUserStore();
-onMounted (()=> {
-  userStore.loggedIn ? null :  useRouter().push({ path: '/login'})
-}
-);
+const config = ref({
+  activePanel: ''
+})
 
-const driverStore = useDriverStore();
-const lookupStore = useLookupStore();
 const activityStore = useActivityStore();
 const interventionStore = useInterventionStore();
-
-const selectedDriver = ref(null);
 const selectedPrjDriver = ref(null);
 
 
-function addIntervention(intervention) {
-  let driver_ids = [];
+function addIntervention(intervention: { name: string, id: number }) {
+  let driver_ids: number[] = [];
   if (selectedPrjDriver.value) {
     driver_ids = [selectedPrjDriver.value.id];
-  } 
+  }
   activityStore.addActivity({ name: intervention.name, driver_ids, intervention_id: intervention.id })
 }
+
+const getInterventions = computed(() => {
+  return interventionStore.interventions.filter((i) => {
+    return i.name != 'Social movements';
+  })
+})
 
 </script>
 
 <template>
-  <section ref="top" class="section">
-    <div class="container">
-      <main>
-        <div
-          v-for="intervention in interventionStore.interventions"
-          :key="intervention.key"
-        >
-        <div v-if="intervention.name != 'Social movements'">
-          <div class="level">
-            <div class="level-left">
-              <div class="level-item has-text-weight-bold">
-                {{ intervention.name }}
-              </div>
-            </div>
-            <div class="level-right">
-              <div class="level-item">
-                <button
-                  v-if="!interventionStore.activityIds(intervention.id).length"
-                  @click="addIntervention(intervention)"
-                  class="button is-success"
-                >
-                  <span class="icon is-small">
-                    <i class="fas fa-check"></i>
-                  </span>
-                  <span>Add</span>
-                </button>
-                <button
-                  v-if="interventionStore.activityIds(intervention.id).length"
-                  @click="activityStore.deleteIntervention(intervention.id)"
-                  class="button is-danger is-outlined "
-                >
-                  <span class="icon is-small">
-                    <i class="fas fa-times"></i>
-                  </span>
-                  <span>Remove</span>
-                </button>
-              </div>
-            </div>
-          </div>
-          <p class="is-italic">{{ intervention.text_short }}</p>
-          <br />
-          <p>{{ intervention.text_long }}</p>
-          <br />
-          <br />
-        </div>
-        </div>
-      </main>
-    </div>
+  <section class="section">
+    <Collapse v-model:activeKey="config.activePanel">
+
+      <CollapsePanel v-for="intervention in getInterventions" :key="intervention.key" :header="intervention.name">
+        <template #extra>
+          <Button v-if="!interventionStore.activityIds(intervention.id).length" @click="addIntervention(intervention)"
+            type="primary" size="small" :ghost="true">
+            <template #icon>
+              <CheckCircleOutlined />
+            </template>
+            Add
+          </Button>
+
+          <Button v-if="interventionStore.activityIds(intervention.id).length"
+            @click="activityStore.deleteIntervention(intervention.id)" type="ghost" :danger="true" size="small">
+            <template #icon>
+              <DeleteOutlined />
+            </template>
+            Remove
+          </Button>
+
+        </template>
+
+        <p class="is-italic">{{ intervention.text_short }}</p>
+        <br />
+        <p>{{ intervention.text_long }}</p>
+      </CollapsePanel>
+
+    </Collapse>
   </section>
 </template>
-
-<style>
-.vertical-center {
-  margin: 2rem;
-}
-</style>
-<!-- 
- -->
