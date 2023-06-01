8aeaa69b8d1beaa78cc59dffb7af9b7329c68ec4
Lawrence Ephrim <ephrimlawrence@gmail.com>
2023-06-01 11:56:16 +0000

    refactor(activities): replace table with ant table component

--- frontend/src/stores/activities.js
+++ frontend/src/stores/activities.js
@@ -1,50 +1,79 @@
-import { defineStore } from 'pinia';
-import { useUserStore } from './user';
-import * as api from '../apis/lambda';
+import { defineStore } from "pinia";
+import { useUserStore } from "./user";
+import * as api from "../apis/lambda";
 
 const init_objects = {
-  activities: {id:0, name:'', parent_id:0, intervention_id:0, driver_ids:[],
-              editing_user_id:0, owner_id:0, status_id:0, notes:'', url:''},
-  schedules:  {id:0, editing_user_id:0, activity_id:0, planned_date_from: new Date(2007,9,26), planned_date_to: new Date(2022,9,26),
-              actual_date_from: new Date(2007,9,26), actual_date_to: new Date(2022,9,26),
-              dependency_ids:[], owner_id:0, participant_id: 0, status_id: 0, notes:'', url: ''}
-}
-
+  activities: {
+    id: 0,
+    name: "",
+    parent_id: 0,
+    intervention_id: 0,
+    driver_ids: [],
+    editing_user_id: 0,
+    owner_id: 0,
+    status_id: 0,
+    notes: "",
+    url: "",
+  },
+  schedules: {
+    id: 0,
+    editing_user_id: 0,
+    activity_id: 0,
+    planned_date_from: new Date(2007, 9, 26),
+    planned_date_to: new Date(2022, 9, 26),
+    actual_date_from: new Date(2007, 9, 26),
+    actual_date_to: new Date(2022, 9, 26),
+    dependency_ids: [],
+    owner_id: 0,
+    participant_id: 0,
+    status_id: 0,
+    notes: "",
+    url: "",
+  },
+};
 
 export const useActivityStore = defineStore({
-  id: 'activities',
+  id: "activities",
   state: () => ({
     activities: [],
-    schedules: []
+    schedules: [],
   }),
   getters: {
-    driverInActivities:(state) =>
-      (driverId) => state.activities.filter((a)=>a.driver_ids.includes(driverId)).length > 0, 
-    activityById:(state) => 
-      (activityId) => state.activities.find((a)=>a.id==activityId),
-    topLevelActivities:(state) => state.activities.filter((a)=>a.parent_id==null),
-    subActivitiesByActivityId:(state) => 
-      (activityId) => state.activities.filter((a)=>a.parent_id==activityId),
-    schedulesByActivityId:(state) =>
-      (activityId) => state.schedules.filter((s)=>s.activity_id==activityId),
-    fromDate:(state) =>
-      (activityId) => {
-        const schedules = state.schedules.filter((s)=>s.activity_id==activityId)
-        let date = ''
-        if (schedules.length > 0) {
-          date = schedules.reduce((prev,cur)=>(cur.planned_date_from < prev.planned_date_from) ? cur : prev).planned_date_from;
-        }
-        return date;
-      },
-    toDate:(state) =>
-      (activityId) => {
-        const schedules = state.schedules.filter((s)=>s.activity_id==activityId)
-        let date = ''
-        if (schedules.length > 0) {
-          date = schedules.reduce((prev,cur)=>(cur.planned_date_to > prev.planned_date_to) ? cur : prev).planned_date_to;
-        }
-        return date;
+    driverInActivities: (state) => (driverId) =>
+      state.activities.filter((a) => a.driver_ids.includes(driverId)).length >
+      0,
+    activityById: (state) => (activityId) =>
+      state.activities.find((a) => a.id == activityId),
+    topLevelActivities: (state) =>
+      state.activities.filter((a) => a.parent_id == null),
+    subActivitiesByActivityId: (state) => (activityId) =>
+      state.activities.filter((a) => a.parent_id == activityId),
+    schedulesByActivityId: (state) => (activityId) =>
+      state.schedules.filter((s) => s.activity_id == activityId),
+    fromDate: (state) => (activityId) => {
+      const schedules = state.schedules.filter(
+        (s) => s.activity_id == activityId
+      );
+      let date = "";
+      if (schedules.length > 0) {
+        date = schedules.reduce((prev, cur) =>
+          cur.planned_date_from < prev.planned_date_from ? cur : prev
+        ).planned_date_from;
       }
+      return date;
+    },
+    toDate: (state) => (activityId) => {
+      const schedules = state.schedules.filter(
+        (s) => s.activity_id == activityId
+      );
+      let date = "";
+      if (schedules.length > 0) {
+        date = schedules.reduce((prev, cur) =>
+          cur.planned_date_to > prev.planned_date_to ? cur : prev
+        ).planned_date_to;
+      }
+      return date;
+    },
   },
   actions: {
     clear() {
@@ -53,62 +82,67 @@ export const useActivityStore = defineStore({
       }
     },
     download() {
-      api.downloadObjects(init_objects,this,'',true);
+      api.downloadObjects(init_objects, this, "", true);
     },
 
-    async addActivity (activity) {
+    async addActivity(activity) {
       delete activity.id;
-      const table = 'activities';
-      const newId = await api.insert(table, {...activity});   //const newId = 99; // 
+      const table = "activities";
+      const newId = await api.insert(table, { ...activity }); //const newId = 99; //
       activity.id = newId;
       this.activities.push(activity);
     },
 
-    updateActivity (activity) {
+    updateActivity(activity) {
       // console.log(activity);
-      let idx = this.activities.findIndex((a)=>a.id==activity.id);
-      this.activities.splice(idx,1,activity);    
-      api.update('activities',activity.id, {...activity});
+      let idx = this.activities.findIndex((a) => a.id == activity.id);
+      this.activities.splice(idx, 1, activity);
+      api.update("activities", activity.id, { ...activity });
     },
 
-    async deleteActivity (activityId, deleteChildren = false) {
+    async deleteActivity(activityId, deleteChildren = false) {
       // find activity to be deleted
-      const activity = this.activities.find((a)=>a.id===activityId); 
+      const activity = this.activities.find((a) => a.id === activityId);
 
       // handle children of the to-be-deleted activity
-      const children = this.activities.filter((a)=>a.parent_id===activityId);
+      const children = this.activities.filter(
+        (a) => a.parent_id === activityId
+      );
       const deleteIds = [].push(activity).push(children);
       // console.log('deleteIds:');
       // console.log(deleteIds);
-      const table = 'activities';
-      api.remove(table,deleteIds);
+      const table = "activities";
+      api.remove(table, deleteIds);
       if (deleteChildren)
-        this.activities = this.activities.filter((a)=>!children.includes(a));
-      else 
-        children.forEach((c)=>c.parent_id = activity.parent_id);
-      this.activities = this.activities.filter((a)=> a != activity)
+        this.activities = this.activities.filter((a) => !children.includes(a));
+      else children.forEach((c) => (c.parent_id = activity.parent_id));
+      this.activities = this.activities.filter((a) => a != activity);
     },
 
     async deleteIntervention(interventionId) {
       // Delete an activity from an intervention id ONLY IF:
-      //    - it has no schedules 
+      //    - it has no schedules
       //    - it has no notes
       //    - it has no child activities
       // If these conditions are not met, return false.  deleteActivity() should be used instead.
-      
+
       // find all activities linked to the intervention
-      const removeActivities = this.activities.filter((a,idx,arr)=> 
-          a.intervention_id===interventionId && 
-          this.schedules.filter((s)=>s.activity_id==a.id).length == 0 &&
-          (!a.notes || a.notes=='') &&
-          !arr.filter((b)=>b.parent_id==a.id).length);
-      const deleteIds = removeActivities.map((a)=>a.id);
+      const removeActivities = this.activities.filter(
+        (a, idx, arr) =>
+          a.intervention_id === interventionId &&
+          this.schedules.filter((s) => s.activity_id == a.id).length == 0 &&
+          (!a.notes || a.notes == "") &&
+          !arr.filter((b) => b.parent_id == a.id).length
+      );
+      const deleteIds = removeActivities.map((a) => a.id);
       // console.log('deleteIds:');
       // console.log(deleteIds);
-      const table = 'activities';
-      api.remove(table,deleteIds);
-    
-      this.activities = this.activities.filter((a)=>!removeActivities.includes(a));
-    }
-  }
-})
+      const table = "activities";
+      api.remove(table, deleteIds);
+
+      this.activities = this.activities.filter(
+        (a) => !removeActivities.includes(a)
+      );
+    },
+  },
+});
--- frontend/src/views/Activities.vue
+++ frontend/src/views/Activities.vue
@@ -9,6 +9,8 @@ import { useParticipantStore } from '@/stores/participants'
 import { useUserStore } from "@/stores/user";
 import { useRouter } from 'vue-router'
 import AddActivityModal from '../components/AddActivityModal.vue';
+import { Button, Table, Tag, Typography } from "ant-design-vue";
+import type { PlusCircleOutlined } from "@ant-design/icons-vue";
 
 const showAddModal = ref(false);
 const showEditModal = ref(false);
@@ -37,12 +39,131 @@ function editActivity(activity) {
   showEditModal.value = true;
 }
 
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+  },
+  {
+    title: 'Owner',
+    dataIndex: 'Owner',
+    key: 'owner',
+  },
+  {
+    title: 'Duration',
+    dataIndex: 'duration',
+    key: 'duration',
+  },
+  // {
+  //   title: 'To',
+  //   dataIndex: 'to',
+  //   key: 'to',
+  // },
+  {
+    title: 'Intervention',
+    dataIndex: 'intervention',
+    key: 'intervention',
+  },
+  {
+    title: 'Drivers',
+    dataIndex: 'drivers',
+    key: 'drivers',
+  },
+]
 </script>
 
 <template>
-  <button class="button is-primary m-4" @click.prevent="showAddModal = true">
+  <section class="section">
+    <Table :columns="columns" :data-source="activityStore.topLevelActivities" bordered>
+      <template #title>
+        <div class="level">
+          <div class="level-left">
+            <Typography :level="3">Project Activities</Typography>
+          </div>
+
+          <div class="level-right">
+            <Space>
+              <Button type="primary" @click="showAddModal = true">
+                <template #icon>
+                  <PlusCircleOutlined />
+                </template>
+                Add Activity
+              </Button>
+
+              <!-- <Button type="ghost" @click="config.settingsModal.visible = true">
+                <template #icon>
+                  <SettingOutlined />
+                </template>
+                Settings
+              </Button> -->
+            </Space>
+          </div>
+        </div>
+      </template>
+
+      <template #bodyCell="{ column, record: activity }">
+        <template v-if="column.key === 'name'">
+          <a @click="editActivity(activity)">{{ activity.name }}</a>
+
+          <!-- TODO: display subactivities in a modal -->
+          <Tag>sub activities</Tag>
+        </template>
+
+        <template v-if="column.key === 'status'">
+          {{ lookupStore.lookupNameById('activity_status', activity.status_id) }}
+        </template>
+
+        <template v-if="column.key === 'owner'">
+          {{ projectStore.userName(activity.owner_id) }}
+        </template>
+
+        <template v-if="column.key === 'duration'">
+          {{ activityStore.fromDate(activity.id) }} - {{ activityStore.toDate(activity.id) }}
+        </template>
+
+        <template v-if="column.key === 'intervention'">
+          {{ interventionStore.interventionNameById(activity.intervention_id) }}
+        </template>
+
+        <template v-if="column.key === 'drivers'">
+          <span v-if="activity.driver_ids && activity.driver_ids.length == 1">{{
+            driverStore.nameById(activity.driver_ids[0]) }}
+          </span>
+
+          <span v-else>
+            <i>{{ activity.driver_ids.length }} drivers</i>
+          </span>
+        </template>
+      </template>
+    </Table>
+
+    <!-- TODO: display sub activites in a modal -->
+    <!-- <template v-if="expandActivity.includes(activity.id)">
+      <tr v-for="subActivity in activityStore.subActivitiesByActivityId(activity.id)" class="has-text-weight-normal">
+        <td></td>
+        <td>{{ subActivity.id }}</td>
+        <td><span class="mx-2"><a @click="editActivity(subActivity)">{{ subActivity.name }}</a></span></td>
+        <td>{{ lookupStore.lookupNameById('activity_status', subActivity.status_id) }}</td>
+        <td>{{ projectStore.userName(subActivity.owner_id) }}</td>
+        <td>{{ activityStore.fromDate(subActivity.id) }}</td>
+        <td>{{ activityStore.toDate(subActivity.id) }}</td>
+        <td>{{ interventionStore.interventionNameById(subActivity.intervention_id) }}</td>
+        <td v-if="subActivity.driver_ids && subActivity.driver_ids.length == 1">
+          {{ driverStore.nameById(subActivity.driver_ids[0]) }}</td>
+        <td v-else><i>{{ subActivity.driver_ids.length }} drivers</i></td>
+      </tr>
+    </template> -->
+  </section>
+
+  <!-- <button class="button is-primary m-4" @click.prevent="showAddModal = true">
     <span v-if="!showAddModal">Add</span>
-  </button>
+  </button> -->
 
   <!-- <button class="button" @click.prevent = "showEditModal = true">
     <span v-if="!showEditModal">Edit Activity</span>
@@ -50,7 +171,7 @@ function editActivity(activity) {
   <AddActivityModal v-if="showAddModal" :draft-activity="emptyActivity" v-model="showAddModal" />
   <AddActivityModal v-if="showEditModal" :draft-activity="draftActivity" v-model="showEditModal" />
 
-  <table class="table">
+  <!-- <table class="table">
     <thead>
       <tr>
         <th></th>
@@ -68,19 +189,29 @@ function editActivity(activity) {
       <template v-for="activity in activityStore.topLevelActivities">
         <tr class="has-text-weight-medium">
           <td v-if="activityStore.subActivitiesByActivityId(activity.id).length == 0"></td>
-          <td v-else-if="!expandActivity.includes(activity.id)"><a @click="expandActivity.push(activity.id)">></a></td>
+
+          <td v-else-if="!expandActivity.includes(activity.id)">
+            <a @click="expandActivity.push(activity.id)"></a>
+          </td>
+
           <td v-else><a @click="expandActivity.splice(expandActivity.indexOf(activity.id), 1)">V</a></td>
+
           <td>{{ activity.id }}</td>
+
           <td><a @click="editActivity(activity)">{{ activity.name }}</a></td>
+
           <td>{{ lookupStore.lookupNameById('activity_status', activity.status_id) }}</td>
+
           <td>{{ projectStore.userName(activity.owner_id) }}</td>
           <td>{{ activityStore.fromDate(activity.id) }}</td>
           <td>{{ activityStore.toDate(activity.id) }}</td>
           <td>{{ interventionStore.interventionNameById(activity.intervention_id) }}</td>
-          <td v-if="activity.driver_ids && activity.driver_ids.length == 1">{{ driverStore.nameById(activity.driver_ids[0]) }}
+          <td v-if="activity.driver_ids && activity.driver_ids.length == 1">{{
+            driverStore.nameById(activity.driver_ids[0]) }}
           </td>
           <td v-else><i>{{ activity.driver_ids.length }} drivers</i></td>
         </tr>
+
         <template v-if="expandActivity.includes(activity.id)">
           <tr v-for="subActivity in activityStore.subActivitiesByActivityId(activity.id)" class="has-text-weight-normal">
             <td></td>
@@ -98,7 +229,7 @@ function editActivity(activity) {
         </template>
       </template>
     </tbody>
-  </table>
+  </table> -->
 </template>
 
 <style>
