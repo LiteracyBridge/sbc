1686be349b24a052a1e0d71de6faa82bcf7e77f3
Lawrence Ephrim <ephrimlawrence@gmail.com>
2023-06-13 07:55:37 +0000

    ci: update amplify hosting

--- frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,10 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "hosting": {
     "S3AndCloudFront": {
-      "CloudFrontDistributionID": "string",
-      "CloudFrontDomainName": "string",
-      "CloudFrontOriginAccessIdentity": "string",
-      "CloudFrontSecureURL": "string",
       "HostingBucketName": "string",
       "Region": "string",
       "S3BucketSecureURL": "string",
