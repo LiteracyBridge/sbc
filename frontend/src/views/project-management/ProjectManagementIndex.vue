ed0083318053242b45399658218246dc556bf158
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2023-06-23 15:10:58 +0000

    feat: add risk mitigation component

--- frontend/src/views/project-management/ProjectManagementIndex.vue
+++ frontend/src/views/project-management/ProjectManagementIndex.vue
@@ -3,7 +3,7 @@ import { onMounted, ref, reactive, watch } from "vue";
 import { Button, TabPane, Table, Tabs, Tag, Typography } from "ant-design-vue";
 
 import ActivitiesTable from "./activities/ActivitiesTable.vue";
-
+import RiskMitigation from "./RiskMitigation.vue";
 
 const config = ref({
   activeTab: 'activities'
@@ -17,6 +17,9 @@ const config = ref({
     <Tabs v-model:activeKey="config.activeTab" centered class="my-3 mx-3">
       <TabPane key="activities" tab="Activities">
         <ActivitiesTable></ActivitiesTable>
+      </TabPane>
+      <TabPane key="risk" tab="Risk Mitigation Strategy">
+        <RiskMitigation></RiskMitigation>
       </TabPane>
     </Tabs>
   </section>
---
+++ frontend/src/views/project-management/RiskMitigation.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { onMounted, ref, reactive, } from "vue";
+import { useActivityStore } from "@/stores/activities";
+import { useInterventionStore } from "@/stores/interventions";
+import { useDriverStore } from "@/stores/drivers";
+import { useLookupStore } from "@/stores/lookups";
+import { useProjectStore } from '@/stores/projects'
+import { Button, Space, Spin, Table, Tag, Typography } from "ant-design-vue";
+import { PlusCircleOutlined } from "@ant-design/icons-vue";
+
+// import AddActivityModal from './AddActivityModal.vue';
+// import SubActivitiesDrawer from "./SubActivitiesDrawer.vue";
+import { Activity } from "@/types";
+
+const config = ref({
+  loading: false,
+  selectedActivity: null,
+  subActivityModal: { visible: false },
+});
+
+const columns = [
+  {
+    title: 'Theory of Change Item',
+    key: 'toc',
+  },
+  {
+    title: 'Risk',
+    key: 'risk',
+  },
+  {
+    title: 'Strategy',
+    key: 'strategy',
+  },
+]
+</script>
+
+<template>
+  <Spin :spinning="config.loading">
+
+    <Table :columns="columns" :data-source="[]" bordered>
+      <!-- <template #title>
+        Risk Mitigation Strategy
+      </template> -->
+
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.key === 'name'">
+
+        </template>
+
+      </template>
+    </Table>
+  </Spin>
+</template>
+
+<style>
+</style>
