01e321d2301c6728ec09af76fcf8c44804b54699
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2023-06-27 15:06:43 +0000

    fix: disable sentry

--- backend/app.py
+++ backend/app.py
@@ -26,13 +26,13 @@ import models
 from config import settings
 
 
-sentry_sdk.init(
-    dsn=settings.sentry_dsn,
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production,
-    # traces_sample_rate=1.0,
-)
+# sentry_sdk.init(
+#     dsn=settings.sentry_dsn,
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for performance monitoring.
+#     # We recommend adjusting this value in production,
+#     # traces_sample_rate=1.0,
+# )
 
 
 # from database import SessionLocal, engine
