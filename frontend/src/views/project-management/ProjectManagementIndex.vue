db6b97643e85a2aa2a0bad83e83514defce3f3bc
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2023-06-27 16:07:04 +0000

    fix(ui): bug fix

--- frontend/src/views/project-management/ProjectManagementIndex.vue
+++ frontend/src/views/project-management/ProjectManagementIndex.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { onMounted, ref, reactive, watch } from "vue";
-import { Button, TabPane, Table, Tabs, Tag, Typography } from "ant-design-vue";
+import { Button, TabPane, Card, Tabs, Tag, Typography } from "ant-design-vue";
 
 import ActivitiesTable from "./activities/ActivitiesTable.vue";
 import RiskMitigation from "./RiskMitigation.vue";
@@ -14,8 +14,8 @@ const config = ref({
 </script>
 
 <template>
-  <section class="section">
-    <Tabs v-model:activeKey="config.activeTab" centered class="my-3 mx-3">
+  <Card title="Project Management">
+    <Tabs v-model:activeKey="config.activeTab" centered>
       <TabPane key="activities" tab="Activities">
         <ActivitiesTable></ActivitiesTable>
       </TabPane>
@@ -26,7 +26,7 @@ const config = ref({
         <SustainabilityStrategy></SustainabilityStrategy>
       </TabPane>
     </Tabs>
-  </section>
+  </Card>
 </template>
 
 <style>
