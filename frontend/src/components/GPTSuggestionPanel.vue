4a1aaaf209c51c4d982639a002730c3d63792781
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2023-07-25 08:21:47 +0000

    refactor(suggestions-panel): update modal components

--- frontend/src/components/GPTSuggestionPanel.vue
+++ frontend/src/components/GPTSuggestionPanel.vue
@@ -4,37 +4,38 @@ import { ref, computed, watch } from "vue";
 import { useProjectDataStore } from "@/stores/projectData";
 import * as lambda from "@/apis/lambda";
 import PulseLoaderVue from "./spinners/PulseLoader.vue";
-import { Drawer, Button, Space } from "ant-design-vue";
+import { Drawer, Form, Button, Space, FormItem, Textarea, Row, Col } from "ant-design-vue";
+import { CheckOutlined } from "@ant-design/icons-vue";
 
-const emit = defineEmits(['isClosed']);
+const emit = defineEmits<{
+  (e: "isClosed"): void;
+  (e: "saved", data: string): string;
+}>();
 
-
-const props = defineProps({
-  module: {
-    type: String,
-    required: true,
-  },
-  questionId: {
-    type: Number,
-    required: false,
-  },
-  isVisible: {
-    type: Boolean,
-    required: true,
-  }
-});
+const props = defineProps<{
+  module?: string;
+  questionId?: number;
+  ai?: {
+    prompt: string;
+    context: string;
+    format: string;
+  };
+  isVisible: boolean;
+}>();
 
 const projectStore = useProjectDataStore();
 
 const gptResponse = ref({ answer: null, isLoading: false });
 const formInput = ref("");
 const config = ref({
-  visible: props.isVisible
-})
+  visible: props.isVisible,
+});
 
 const moduleQuestion = computed(() => {
-  return projectStore.questionsForTopic(props.module).find(i => i.id == props.questionId);
-})
+  return projectStore
+    .questionsForTopic(props.module)
+    .find((i) => i.id == props.questionId);
+});
 
 // FIXME: Rewrite submitContextAndPrompt function with more clarity
 async function submitContextAndPrompt() {
@@ -44,20 +45,27 @@ async function submitContextAndPrompt() {
   _gptResp.isLoading = true;
   gptResponse.value = _gptResp;
 
+  let ai_answer = null;
+  if (props.ai == null) {
+    let context = "";
+    let qToFill = null;
+    for (var q of projectStore.questions) {
+      const a = projectStore.getData(q.id);
+      if (q.id == id) {
+        qToFill = q;
+      } else if (!(a == null || a == "")) {
+        context += q.label + ":" + a + "\n";
+      }
+    }
 
-  let context = "";
-  let qToFill = null;
-  for (var q of projectStore.questions) {
-    const a = projectStore.getData(q.id);
-    if (q.id == id) {
-      qToFill = q
-    }
-    else if (!(a == null || a == "")) {
-      context += q.label + ":" + a + "\n";
-    }
+    ai_answer = await lambda.gptCompletion(qToFill.q2ai, context, qToFill.f4ai);
+  } else {
+    ai_answer = await lambda.gptCompletion(
+      props.ai.prompt,
+      props.ai.context,
+      props.ai.format
+    );
   }
-
-  const ai_answer = await lambda.gptCompletion(qToFill.q2ai, context, qToFill.f4ai);
 
   gptResponse.value.answer = ai_answer;
   gptResponse.value.isLoading = false;
@@ -67,36 +75,50 @@ const closeButton = () => {
   gptResponse.value.answer = null;
   formInput.value = "";
 
-  emit("isClosed", true);
+  emit("isClosed");
 };
 
-watch(props, (newProps) => {
-  config.value.visible = newProps.isVisible;
+watch(
+  props,
+  (newProps) => {
+    config.value.visible = newProps.isVisible;
+    gptResponse.value.answer = null;
 
-  submitContextAndPrompt();
+    submitContextAndPrompt();
 
-  formInput.value = projectStore.getData(newProps.questionId);
-  gptResponse.value.answer = null;
-}, { deep: true });
-
+    if (newProps.ai == null) {
+      formInput.value = projectStore.getData(newProps.questionId);
+    }
+  },
+  { deep: true }
+);
 
 function onInputChange(event: any) {
   formInput.value = event.target.value;
 }
-
 </script>
 
 <template>
   <!-- TODO: change to ant drawer -->
-  <Drawer :visible="config.visible" :mask-closable="false" width="80vw" transition-name="slide-right" @close="closeButton">
+  <Drawer
+    v-model:open="config.visible"
+    :mask-closable="false"
+    width="80vw"
+    @close="closeButton"
+  >
     <!-- <div class="level">
       <div class="level-left"></div>
       <div class="level-right mt-2 mr-4"> -->
 
     <template #extra>
       <Space>
-
-        <Button type="primary" @click="projectStore.setData(props.questionId, formInput); closeButton()">
+        <Button
+          type="primary"
+          @click="
+            projectStore.setData(props.questionId, formInput);
+            closeButton();
+          "
+        >
           <!-- <span class="icon mr-1">
                 <i class="fas fa-save"></i>
               </span> -->
@@ -116,10 +138,8 @@ function onInputChange(event: any) {
       </div>
     </div> -->
 
-    <div class="columns my-3 mx-5">
-
-
-      <div class="column is-4 mt-2">
+    <Row :gutter="8">
+      <Col :span="10">
         <div class="mt-2">
           <!-- Display loading indicator -->
           <div v-if="gptResponse?.isLoading == true">
@@ -129,44 +149,49 @@ function onInputChange(event: any) {
           </div>
 
           <p v-else style="white-space: pre-wrap">
-            {{ gptResponse?.answer || 'No suggestions available. Click on the light bulb to see suggestions' }}
+            {{
+              gptResponse?.answer ||
+              "No suggestions available. Click on the light bulb to see suggestions"
+            }}
           </p>
 
           <div class="field is-grouped mt-3">
             <div class="control">
               <!-- FIXME: hide this button if chatgpt throws error -->
-              <Button type="primary" @click="formInput = formInput + '\n\n' + gptResponse?.answer;"
-                :class="{ 'disabled': gptResponse.isLoading || gptResponse?.answer == undefined }"
-                :disabled="gptResponse.isLoading || gptResponse?.answer == undefined">
-                <span class="icon is-small mr-1">
-                  <i class="fas fa-check"></i>
-                </span>
+              <Button
+                type="primary"
+                @click="formInput = formInput + '\n\n' + gptResponse?.answer"
+                :class="{
+                  disabled: gptResponse.isLoading || gptResponse?.answer == undefined,
+                }"
+                :disabled="gptResponse.isLoading || gptResponse?.answer == undefined"
+              >
+                <CheckOutlined />
 
                 Accept
               </Button>
             </div>
 
             <div class="control">
-              <Button @click="submitContextAndPrompt()" :class="{ 'is-loading disabled': gptResponse.isLoading }"
-                :disabled="gptResponse.isLoading">
+              <Button
+                @click="submitContextAndPrompt()"
+                :class="{ 'is-loading disabled': gptResponse.isLoading }"
+                :disabled="gptResponse.isLoading"
+              >
                 Refresh
               </Button>
             </div>
           </div>
         </div>
-      </div>
+      </Col>
 
-      <div class="column">
-        <div class="field">
-          <label class="label" :for="`input`">{{ moduleQuestion?.q2u }}</label>
-
-          <div class="control">
-            <textarea class="textarea" @change="onInputChange($event)" :value="formInput" rows="15" cols="10"></textarea>
-          </div>
-        </div>
-      </div>
-
-    </div>
-
+      <Col :span="14">
+        <Form layout="vertical">
+          <FormItem :label="moduleQuestion?.q2u">
+            <Textarea v-model:value="formInput" rows="15" cols="10"></Textarea>
+          </FormItem>
+        </Form>
+      </Col>
+    </Row>
   </Drawer>
 </template>
